4496e95ffb42ae128f703d93d3437f0a51fb2464
bayuokta463 <bayuokta463@gmail.com>
2021-01-22 18:11:34 +0700

    add leak Canary

--- app/src/main/java/com/bayuokta/movieapp/MainActivity.kt
+++ app/src/main/java/com/bayuokta/movieapp/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.bayuokta.movieapp
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.bayuokta.movieapp.about.AboutActivity
@@ -13,6 +17,7 @@ import com.bayuokta.movieapp.home.HomeFragment
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var broadcastReceiver: BroadcastReceiver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +32,41 @@ class MainActivity : AppCompatActivity() {
                 .commit()
             supportActionBar?.title = getString(R.string.app_name)
         }
+    }
+
+    private fun registerBroadCastReceiver(){
+        broadcastReceiver = object : BroadcastReceiver(){
+            override fun onReceive(context: Context?, intent: Intent?) {
+                when (intent?.action) {
+                    Intent.ACTION_POWER_CONNECTED -> {
+                        Toast.makeText(this@MainActivity, R.string.power_connected,
+                            Toast.LENGTH_SHORT).show()
+                    }
+                    Intent.ACTION_POWER_DISCONNECTED -> {
+                        Toast.makeText(this@MainActivity, R.string.power_disconnected,
+                            Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+
+        }
+
+        val intentFilter = IntentFilter()
+        intentFilter.apply {
+            addAction(Intent.ACTION_POWER_CONNECTED)
+            addAction(Intent.ACTION_POWER_DISCONNECTED)
+        }
+        registerReceiver(broadcastReceiver, intentFilter)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        registerBroadCastReceiver()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(broadcastReceiver)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
